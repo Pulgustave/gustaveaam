62c6e28c1af90bf0e08c4dcf4f7e1858d1461c71
Gustavo A Avellaneda <pulgustave@gustavos-macbook-pro.home>
2026-06-12 19:06:11 +0200

    Rename project to gustaveaam, update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-web-app/', // Update this to match your repository name unless it's your root domain (e.g., Pulgustave.github.io)
+  base: '/gustaveaam/', // Update this to match your repository name unless it's your root domain (e.g., Pulgustave.github.io)
 })
